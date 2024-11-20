b1d773539d7eba714cbfcec5d8ecb911b044336c
c5514 <c5514@c5514.com>
2024-11-20 06:17:49 -0500

    New colorscheme

--- config/colors/one-dark.nix
+++ config/colors/one-dark.nix
@@ -1,18 +1,18 @@
 _: {
   base00 = "#000000";
   base01 = "#2c2a2a";
-  base02 = "#b7bec9";
+  base02 = "#3e4451";
   base03 = "#878281";
-  base04 = "#57a5e5";
-  base05 = "#b7bec9";
-  base06 = "#d390e7";
-  base07 = "#3e4249";
+  base04 = "#565c64";
+  base05 = "#abb2bf";
+  base06 = "#b6bdca";
+  base07 = "#c8ccd4";
   base08 = "#de5d68";
-  base09 = "#ffffcc";
-  base0A = "#b7bec9";
-  base0B = "#87c914";
-  base0C = "#5ebaa5";
+  base09 = "#c49060";
+  base0A = "#e5c07b";
+  base0B = "#8fb573";
+  base0C = "#51a8b3";
   base0D = "#57a5e5";
-  base0E = "#d390e7";
-  base0F = "#dbb671";
+  base0E = "#c678dd";
+  base0F = "#818387";
 }
--- config/plug/colorscheme/colorscheme.nix
+++ config/plug/colorscheme/colorscheme.nix
@@ -8,19 +8,19 @@
         transparent = true;
       };
     };
-    # base16 = {
-    #   enable = true;
-    #   setUpBar = false;
-    #   colorscheme = import ../../colors/${config.theme}.nix { };
-    #   settings = {
-    #     cmp = true;
-    #     illuminate = true;
-    #     indentblankline = true;
-    #     lsp_semantic = true;
-    #     mini_completion = true;
-    #     telescope = true;
-    #     telescope_borders = false;
-    #   };
-    # };
+    base16 = {
+      enable = true;
+      setUpBar = false;
+      colorscheme = import ../../colors/${config.theme}.nix { };
+      settings = {
+        cmp = true;
+        illuminate = true;
+        indentblankline = true;
+        lsp_semantic = true;
+        mini_completion = true;
+        telescope = true;
+        telescope_borders = false;
+      };
+    };
   };
 }
